8d59daf6b4bfa2d6cc70866019490864f5884c69
Polina Bril <venera_48@mail.ru>
2021-01-15 12:05:26 +0300

    init commit

---
+++ collectionsManager/src/main/java/collectionsManager/CollectionsManagerApplication.java
@@ -0,0 +1,13 @@
+package collectionsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsManagerApplication.class, args);
+	}
+
+}
---
+++ collectionsManager/src/test/java/collectionsManager/CollectionsManagerTest.java
@@ -0,0 +1,13 @@
+package collectionsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CollectionsManagerApplication.class)
+public class CollectionsManagerTest {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
