914e0dd55a223e9ee7dd2c881bef965507494b8e
Polina Bril <venera_48@mail.ru>
2021-02-01 04:51:02 +0300

    config

--- collectionsManager/src/main/java/collectionsManager/CollectionsManagerApplication.java
+++ collectionsManager/src/main/java/collectionsManager/CollectionsManagerApplication.java
@@ -2,8 +2,12 @@ package collectionsManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import collectionsManager.repository.UserRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class CollectionsManagerApplication {
 
 	public static void main(String[] args) {
